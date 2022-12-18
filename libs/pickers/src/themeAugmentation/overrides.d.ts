131b1654a4f0230d2813e553f44b5e4606c1ceab
Xavi <amirabadi.mahdi@gmail.com>
2022-12-18 11:55:30 +0330

    chore: develop DesktopWrapper & PickersPopper

--- libs/pickers/src/themeAugmentation/overrides.d.ts
+++ libs/pickers/src/themeAugmentation/overrides.d.ts
@@ -1,7 +1,11 @@
+import { PickersPopperClassKey } from 'src/internals/components/pickersPopperClasses';
+
 export interface PickersComponentNameToClassKey {
   ZenLocalizationProvider: never;
 }
 
 declare module '@mui/material/styles' {
-  interface ComponentNameToClassKey extends PickersComponentNameToClassKey {}
+  interface ComponentNameToClassKey extends PickersComponentNameToClassKey {
+    ZenPickersPopper: PickersPopperClassKey;
+  }
 }
