baecbc785ec7c9d8fbb469bdd23eec541dfb8938
Mahdi Abdi <mahdiabdif@gmail.com>
2023-01-11 14:58:32 +0330

    feat: change in storybook styles

--- libs/core/src/styles/createTheme.tsx
+++ libs/core/src/styles/createTheme.tsx
@@ -25,13 +25,6 @@ declare module '@mui/material/styles' {
   interface Palette {
     disabled?: string;
   }
-  interface BreakpointOverrides {
-    xs: false; // removes the `xs` breakpoint
-    sm: false;
-    md: false;
-    lg: false;
-    xl: false;
-  }
 }
 
 /**
